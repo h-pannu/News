62d2b481e08c464295d69c26868d4a759847ab9f
HarpreetPannu <er.harpreetpannu@live.com>
2023-10-30 23:33:33 -0700

    1) Updated code to use ObservableObject instead of INotifypropertyChanged. 2) Decorated property name with [Observableproperty]

--- News/ViewModel/NewsViewModel.cs
+++ News/ViewModel/NewsViewModel.cs
@@ -4,36 +4,42 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace News.ViewModel
 {
-    public class NewsViewModel : INotifyPropertyChanged
+    //public class NewsViewModel : INotifyPropertyChanged
+    public partial class NewsViewModel : ObservableObject
     {
-        public Command SignInCommand { get; set; }
-        private string userName;
+        [ObservableProperty]
+        string username;
 
-        public event PropertyChangedEventHandler PropertyChanged;
 
-        public string UserName
-        {
-            get { return userName; }
-            set { userName = value; 
-                OnPropertyChanged("UserName"); }
-        }
+        //public Command SignInCommand { get; set; }
+        //private string userName;
 
-        public NewsViewModel()
-        {
-            SignInCommand = new Command(SignIn);
-        }
+        //public event PropertyChangedEventHandler PropertyChanged;
 
-        private void SignIn(object obj)
-        {
-            var user = UserName;
-        }
+        //public string UserName
+        //{
+        //    get { return userName; }
+        //    set { userName = value; 
+        //        OnPropertyChanged("UserName"); }
+        //}
 
-        void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        //public NewsViewModel()
+        //{
+        //    SignInCommand = new Command(SignIn);
+        //}
+
+        //private void SignIn(object obj)
+        //{
+        //    var user = UserName;
+        //}
+
+        //void OnPropertyChanged(string propertyName)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //}
     }
 }
